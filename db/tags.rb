1ef743de9bbc8b87d58e401d996fc82faa55da1f
Gerald Bauer <gerald.bauer@gmail.com>
2012-11-09 10:35:48 +0100

    add plain text br fixtures; thanks

--- db/tags.rb
+++
@@ -1,8 +0,0 @@
-
-## some tags
-
-continents = ['europe','america','asia','africa','oceania']
-americas = ['north america', 'south america', 'central america', 'caribbean islands']
-orgs = ['un', 'eu', 'mercosur', 'nafta']
-football = ['uefa', 'afc', 'ofc', 'caf', 'csf', 'concacaf'] 
-regions = ['benelux']
